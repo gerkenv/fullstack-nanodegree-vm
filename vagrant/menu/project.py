59476252fa0a29f984d70ee35049d82e38ba0b4f
gerkenv <gerkenv@gmail.com>
2018-05-21 10:51:20 +0200

    Lesson 3.7. Routing with variable in links + list of all restaurants at '/restaurants/.

--- vagrant/menu/project.py
+++ vagrant/menu/project.py
@@ -10,9 +10,9 @@ DBSession = sessionmaker(bind=engine)
 session = DBSession()
 
 @app.route('/')
-@app.route('/hello')
-def HelloWorld():
-    restaurant = session.query(Restaurant).first()
+@app.route('/restaurants/<int:restaurant_id>/')
+def RestaurantMenu(restaurant_id=12):
+    restaurant = session.query(Restaurant).filter_by(id=restaurant_id).one()
     items = session.query(MenuItem).filter_by(restaurant_id=restaurant.id)
     output = ''
     for item in items:
@@ -23,6 +23,19 @@ def HelloWorld():
         output += item.description
         output += '</br>'
         output += '</br>'
+    return output
+
+@app.route('/restaurants')
+def get_all_restaurants(restaurant_id=12):
+    restaurants = session.query(Restaurant).all()
+    output = ''
+    for restaurant in restaurants:
+        output += "<a href='/restaurants/"
+        output += str(restaurant.id)
+        output += "/'>"
+        output += restaurant.name
+        output += "</a>"
+        output += "</br>"
     return output
 
 if __name__ == '__main__':
